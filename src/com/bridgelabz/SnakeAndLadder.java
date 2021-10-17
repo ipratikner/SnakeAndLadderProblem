40281b57265f99a87611f0ff106c7c3363131164
ipratikner <pratiknerlikar@gmail.com>
2021-10-17 09:43:24 +0530

    Added: cheking for options what to do next i.e no play, snake or ladder

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,9 +4,24 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
 
+        //dice rolled to get number between 1 to 6
         int position = 0;
         Random rand = new Random();
-        int diceValue = rand.nextInt(6)+1;
-        System.out.println("Dice Value is ==> " +diceValue);
+        int diceValue = rand.nextInt(6) + 1;
+        System.out.println("Dice Value is ==> " + diceValue);
+
+        //testing options
+        int option = rand.nextInt(3);
+        if (option == 0) {
+            System.out.println("No Play");
+        } else if (option == 1) {
+            System.out.println("Ladder");
+            position += diceValue;
+            System.out.println("Current Position is ==>" + position);
+        } else {
+            System.out.println("Snake");
+            position -= diceValue;
+            System.out.println("Current Position is ==>" + position);
+        }
     }
 }
