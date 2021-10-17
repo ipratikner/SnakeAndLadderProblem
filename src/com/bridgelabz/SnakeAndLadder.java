92b36a02b4d488a0f06f42b47d39b451b73aaedd
ipratikner <pratiknerlikar@gmail.com>
2021-10-17 10:06:19 +0530

    Added: counted total no. of times dice rolled and position after evry rolling

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,8 +5,9 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
 
         int position = 0;
+        int DiceRollCount = 0;
         final int WINNING_POSITION = 100;
-
+        System.out.println("Initial Position ==>" +position);
         //using while loop
         while(position <= WINNING_POSITION) {
             Random rand = new Random();
@@ -14,6 +15,7 @@ public class SnakeAndLadder {
             System.out.println("Dice Value is ==> " +diceValue);
             int option = rand.nextInt(3);
             //check condition in switch
+            DiceRollCount++;
             if(position == WINNING_POSITION) {
                 System.out.println("<====Yoy Won The Game====>");
 
@@ -45,7 +47,9 @@ public class SnakeAndLadder {
                     break;
                 default:
                     System.out.println("Not Correct");
-                }
             }
+            System.out.println("Dice Roll Count ===>"+DiceRollCount);
+
         }
     }
+}
