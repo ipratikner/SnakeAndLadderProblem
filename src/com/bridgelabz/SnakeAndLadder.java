17fe702e045d5bfdf1e1bd3af2a64224b739d3f4
ipratikner <pratiknerlikar@gmail.com>
2021-10-17 09:50:48 +0530

    Added: cheking till winning position 100 comes

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,24 +4,32 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
 
-        //dice rolled to get number between 1 to 6
         int position = 0;
-        Random rand = new Random();
-        int diceValue = rand.nextInt(6) + 1;
-        System.out.println("Dice Value is ==> " + diceValue);
+        final int WINNING_POSITION = 100;
 
-        //testing options
-        int option = rand.nextInt(3);
-        if (option == 0) {
-            System.out.println("No Play");
-        } else if (option == 1) {
-            System.out.println("Ladder");
-            position += diceValue;
-            System.out.println("Current Position is ==>" + position);
-        } else {
-            System.out.println("Snake");
-            position -= diceValue;
-            System.out.println("Current Position is ==>" + position);
+        //using while loop
+        while (position <= WINNING_POSITION) {
+            Random rand = new Random();
+            int diceValue = rand.nextInt(6) + 1;
+            System.out.println("Dice Value is ==> " + diceValue);
+            int option = rand.nextInt(3);
+
+            if (option == 0) {
+                System.out.println("No Play");
+            } else if (option == 1) {
+                System.out.println("Ladder");
+                position += diceValue;
+                System.out.println("Current Position is ==>" + position);
+            } else if (option == 2) {
+                System.out.println("Snake");
+                position -= diceValue;
+                System.out.println("Current Position is ==>" + position);
+
+                if (position < 0) {
+                    position = 0;
+                    System.out.println("Current Position is ==>" + position);
+                }
+            }
         }
     }
 }
