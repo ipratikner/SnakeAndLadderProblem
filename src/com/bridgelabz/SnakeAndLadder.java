789f4ade6506fd404616883fe8a7f36973f53e37
ipratikner <pratiknerlikar@gmail.com>
2021-10-17 09:35:39 +0530

    Added: player rolled dice

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,12 +1,12 @@
 package com.bridgelabz;
-
+import java.util.Random;
 public class SnakeAndLadder {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Snake And Ladder Program...");
-
         int position = 0;
-        System.out.println("Player is at Position: "+position);
+        Random rand = new Random();
+        int diceValue = rand.nextInt(6)+1;
+        System.out.println("Dice Value is ==> " +diceValue);
     }
 }
